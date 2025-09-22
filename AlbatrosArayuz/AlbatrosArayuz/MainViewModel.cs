ff1d7c7e3e8da3a322b4e6292dd3e93a2a7a4427
landren1 <doguoruc888@gmail.com>
2025-09-22 17:59:12 +0300

    Add files via upload

---
+++ AlbatrosArayuz/AlbatrosArayuz/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+
+namespace AlbatrosArayuz
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _takimNo;
+        public string TakimNo
+        {
+            get => _takimNo;
+            set { _takimNo = value; OnPropertyChanged("TakimNo"); }
+        }
+
+        private string _irtifa;
+        public string Irtifa
+        {
+            get => _irtifa;
+            set { _irtifa = value; OnPropertyChanged("Irtifa"); }
+        }
+
+        private string _yonelme;
+        public string Yonelme
+        {
+            get => _yonelme;
+            set { _yonelme = value; OnPropertyChanged("Yonelme"); }
+        }
+
+        private string _dikilme;
+        public string Dikilme
+        {
+            get => _dikilme;
+            set { _dikilme = value; OnPropertyChanged("Dikilme"); }
+        }
+
+        private string _yatis;
+        public string Yatis
+        {
+            get => _yatis;
+            set { _yatis = value; OnPropertyChanged("Yatis"); }
+        }
+
+        private string _wifiBaglantisi;
+        public string WifiBaglantisi
+        {
+            get => _wifiBaglantisi;
+            set { _wifiBaglantisi = value; OnPropertyChanged("WifiBaglantisi"); }
+        }
+
+        private string _sunucuBaglantisi;
+        public string SunucuBaglantisi
+        {
+            get => _sunucuBaglantisi;
+            set { _sunucuBaglantisi = value; OnPropertyChanged("SunucuBaglantisi"); }
+        }
+
+        private string _sistemSaati;
+        public string SistemSaati
+        {
+            get => _sistemSaati;
+            set { _sistemSaati = value; OnPropertyChanged("SistemSaati"); }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AlbatrosArayuz/AlbatrosArayuz/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace AlbatrosArayuz
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // ViewModel'i oluşturup DataContext'e bağla
+            MainViewModel viewModel = new MainViewModel
+            {
+                TakimNo = "99",
+                Irtifa = "12",
+                Yonelme = "33",
+                Dikilme = "5",
+                Yatis = "15",
+                WifiBaglantisi = "1.111..1.111",
+                SunucuBaglantisi = "1.111..1.111",
+                SistemSaati = DateTime.Now.ToShortTimeString()
+            };
+
+            this.DataContext = viewModel;
+        }
+    }
+}
